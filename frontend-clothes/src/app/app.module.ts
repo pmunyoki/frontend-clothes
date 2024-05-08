5ef910978c2980c135447d2a3ade61686ad4955b
pmunyoki <pmunyoki@protonmail.com>
2024-05-08 16:35:22 +0300

    Updates

--- frontend-clothes/src/app/app.module.ts
+++ frontend-clothes/src/app/app.module.ts
@@ -5,20 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { StarRatingComponent } from './star-rating/star-rating.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxStarRatingModule } from 'ngx-star-rating';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent,
-    StarRatingComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgxStarRatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
